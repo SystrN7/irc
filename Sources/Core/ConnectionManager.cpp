6821dbe116b66fd6c946732b169c4dde4947a293
Felix Galaup <felixgalaup@yahoo.fr>
2021-08-19 16:18:24 +0200

    [+]-Add irc resources.

--- Sources/Core/ConnectionManager.cpp
+++ Sources/Core/ConnectionManager.cpp
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 14:52:50 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/08/18 12:49:23 by fgalaup          ###   ########lyon.fr   */
+/*   Updated: 2021/08/19 15:27:49 by fgalaup          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,55 +65,59 @@ void		ConnectionManager::monitorFileDescriptor()
 {
 	this->monitorSocket();
 
-		ifstream ifs("Tests/example_request.http");
+		ifstream ifs("./messges.irc");
 		string content( (istreambuf_iterator<char>(ifs) ),
 				(istreambuf_iterator<char>()));
 
 	while (0 == 0)
 	{
 		if (select(FD_SETSIZE, &this->_read_fds, &this->_write_fds, NULL, NULL) <= 0)
-			throw std::exception();
+			Logging::SystemFatal("[Select]-Fd monitoring failed");
 		
 		// Check socket 
 		for (list<Socket *>::iterator it = this->_registred_socket.begin(); it != this->_registred_socket.end(); it++)
 		{
-			cout << "[+]-[Connection](Client)-New client request." << endl;
+			cout << "Socket" << endl;
 			if (FD_ISSET((*it)->fd_socket, &this->_read_fds))
 			{
+				cout << "[+]-[Connection](Client)-New client request." << endl;
 				Connection	*connection = (*it)->getConnection();
 				this->registerConnection(connection);
 				FD_SET(connection->_fd, &this->_read_fds);
-				FD_SET(connection->_fd, &this->_write_fds);
+				// FD_SET(connection->_fd, &this->_write_fds);
 			}
 		}
-
+		cout << "test" << endl;
 		// Check Connection reading / writing
 		for (list<Connection *>::iterator it = this->_registred_connection.begin(); it != this->_registred_connection.end(); it++)
 		{
+			cout << "Connection" << endl;
 			// Reading
 			if (FD_ISSET((*it)->_fd, &this->_read_fds))
 			{
 				cout << "[<]-(Client)-Recive Data" << endl;
-				cout << (*it)->receiveData() << endl;
-				(*it)->printInfo();
+				cout << *(*it)->receiveData() << endl;
+				// Writing
+				// if(FD_ISSET((*it)->_fd, &this->_write_fds))
+				// {
+					cout << "[>]-(Client)-Send Data." << endl;
+					cout << "Send: " << content << endl;
+					cout << "size of : " << content.size() << endl;
+					(*it)->sendData(content.c_str() , content.size());
+					// FD_SET((*it)->_fd, &this->_read_fds);
+					// // FD_SET((*it)->_fd, &this->_write_fds);
+					// FD_CLR((*it)->_fd, &this->_write_fds);
+					// FD_CLR((*it)->_fd, &this->_read_fds);
+
+					// delete (*it);
+					// this->_registred_connection.remove(*it);
+					// it = this->_registred_connection.begin();
+				// }
 			}
 			
-			// Writing
-			if(FD_ISSET((*it)->_fd, &this->_write_fds))
-			{
-				cout << "[>]-(Client)-Send Data." << endl;
-				(*it)->printInfo();
-				cout << "size of : " << content.size() << endl;
-				(*it)->sendData(content.c_str() , content.size());
-				FD_CLR((*it)->_fd, &this->_write_fds);
-				FD_CLR((*it)->_fd, &this->_read_fds);
-				delete (*it);
-				this->_registred_connection.remove(*it);
-				it = this->_registred_connection.begin();
-			}
 		}
 
 		// this->monitorSocket();
-		this->monitorConnection();
+		// this->monitorConnection();
 	}
 }
