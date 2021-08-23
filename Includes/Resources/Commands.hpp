ff7db564fd53a1ed5e458b4f30dc50ee2f6e0b8e
Sebastien Ruiz <seruiz@z3r6p5.42lyon.fr>
2021-08-23 14:57:11 +0200

    class wip

--- Includes/Resources/Commands.hpp
+++ Includes/Resources/Commands.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Client.hpp                                         :+:      :+:    :+:   */
+/*   Commands.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seruiz <seruiz@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 17:52:45 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/08/23 14:17:33 by seruiz           ###   ########lyon.fr   */
+/*   Updated: 2021/08/23 14:22:02 by seruiz           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,12 @@
 
 using namespace std;
 
-class Client
+class Commands
 {
 	private:
 		/* data */
 		string	_nick;
-		bool	_isOperator;
 	public:
-		Client();
-		Client(string name);
-		Client(string name, bool isop);
-		Client(bool isop);
-		~Client();
+		Commands();
+		~Commands();
 };
---
+++ Sources/Resources/Client.cpp
@@ -0,0 +1,9 @@
+#include "Client.hpp"
+
+Client::Client() : _nick("user"), _isOperator(false) {};
+
+Client::Client(bool isop) : _nick("user"), _isOperator(isop) {};
+
+Client::Client(string name) : _nick(name), _isOperator(false) {};
+
+Client::Client(string name, bool isop) : _nick(name), _isOperator(isop) {};
