85535290f53504a2b79d976ecb9e00ba6b3dad71
Felix Galaup <felixgalaup@yahoo.fr>
2021-08-25 12:00:35 +0200

    [^]-Tempting to add shutdown feature + better core structure.

--- Includes/Core/Core.hpp
+++ Includes/Core/Core.hpp
@@ -6,12 +6,13 @@
 /*   By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 14:48:40 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/08/17 15:05:03 by fgalaup          ###   ########lyon.fr   */
+/*   Updated: 2021/08/25 11:23:16 by fgalaup          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <csignal>
 #include <string>
 #include <iostream>
 #include <sstream>
@@ -23,13 +24,21 @@ using namespace std;
 class Core
 {
 	/* Attribute */
+	public:
+		static bool			_shutdown;
 	private:
+		ConnectionManager	_connection_manager;
+		int					_port_number;
+		string				_password;
 
 	/* Constructor & Destructor */
 	private:
 		Core();
 	public:
 		Core(const string port, const string password, const string existing_network = "");
+		void parsingParams(const string port, const string password, const string existing_network);
+		void showHeader();
+		void start();
 		~Core();
 	
 	/* Operator */
@@ -41,3 +50,6 @@ class Core
 	/* Methode */
 	
 };
+
+// Function to close server
+void	shutdown_server(int code);
