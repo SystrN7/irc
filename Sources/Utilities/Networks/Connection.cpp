a25105f6fe9b843cf561212fe477deae7c12f289
Felix Galaup <felixgalaup@yahoo.fr>
2021-08-27 13:49:58 +0200

    [+]-fix char add.

--- Sources/Utilities/Networks/Connection.cpp
+++ Sources/Utilities/Networks/Connection.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Connection.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seruiz <seruiz@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 15:23:06 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/08/27 11:22:16 by seruiz           ###   ########lyon.fr   */
+/*   Updated: 2021/08/27 13:49:15 by fgalaup          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,6 @@ int		Connection::sendResponce(Responce &message)
  **/
 list<Request *>Connection::receiveRequest()
 {
-	//size_t	i = 0;
 	char	buffer[10000] = {0};
 	ssize_t	length = 0;
 	string	message;
@@ -65,33 +64,29 @@ list<Request *>Connection::receiveRequest()
 
 	message.append(buffer, length);
 
-	/*
-	while ((i = message.find("\n", i)) != string::npos)
-	{
-		if (message[i] != '\r')
-			message.insert(i, 1, '\r');
-		i += 2;
-	}
-	*/
-
-/*
-	while ((message_lenght = message.find("\r\n")) != string::npos)
-	{
-		message_lenght += 2;
-		if (message_lenght > 512)
-			Logging::Warning("[Connection] - Message from client was refused because it exceeded the maximum length of 512 characters.");
-		else
-			message_queue.push_back(new Request(*this, message.substr(0, message_lenght)));
-		message.erase(0, message_lenght);
-	}
-*/
+	
 	while ((message_lenght = message.find("\n")) != string::npos)
 	{
 		message_lenght += 1;
 		if (message_lenght > 512)
 			Logging::Warning("[Connection] - Message from client was refused because it exceeded the maximum length of 512 characters.");
 		else
-			message_queue.push_back(new Request(*this, message.substr(0, message_lenght)));
+		{
+			string unic_message = message.substr(0, message_lenght);
+
+			// cout << "Messages lenght = " << message_lenght << " true messages lenght = " << unic_message.size() << endl;
+			// cout << "\\n = " << (int)'\n' << " end = "  << (int)unic_message[(int)(unic_message.size() - 1)] << endl;
+			// cout << "\\r = " << (int)'\r' << " end = "  << (int)unic_message[(int)(unic_message.size() - 2)]  << endl;
+
+			if (unic_message[(int)(unic_message.size() - 2)] != '\r')
+				unic_message.insert(message_lenght - 1, 1, '\r');
+
+			// cout << "k = " << (int)'k' << " end = "  << (int)unic_message[(int)(unic_message.size() - 3)]  << endl;
+			// cout << "\\r = " << (int)'\r' << " end = "  << (int)unic_message[(int)(unic_message.size() - 2)]  << endl;
+
+			message_queue.push_back(new Request(*this, unic_message));
+		}
+
 		message.erase(0, message_lenght);
 	}
 
